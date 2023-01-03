4983b9be513aedc74d24defa1870fbc6e1995b3c
Kirill Novikov <kirill.novikov@gmail.com>
2023-01-03 10:21:57 +0300

    Add useEffect, routes, links

---
+++ src/GroupedTeamMembers.jsx
@@ -0,0 +1,13 @@
+﻿const GroupedTeamMembers = () => {
+  return (
+    <div className='container'>
+      <div className='row justify-content-center mt-3 mb-4'>
+        <div className='col-8'>
+          <h1>404 - Page Not Found</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GroupedTeamMembers;
